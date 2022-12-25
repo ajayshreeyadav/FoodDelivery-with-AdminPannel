05f66e3f62395f71522eef6a381d9b46429d9e33
ajayshree <ajayshreeyadav@gmail.com>
2022-12-25 20:23:58 +0530

    Added Cart Page and Authentication page

--- server.js
+++ server.js
@@ -7,14 +7,32 @@ const PORT = process.env.PORT || 3000;
 
 // Assets
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
 // set Template Engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+// Home page route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Cart page route
+app.get('/cart', (req, res) => {
+  res.render('customers/cart');
+});
+
+// Login page route
+app.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+// Register page route
+app.get('/register', (req, res) => {
+  res.render('auth/register');
+});
+
 app.listen(PORT, () => {
   console.log(`Listning on port ${PORT}`);
 });
